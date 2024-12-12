5f5fc6ba9bd7cb50c66523e14bca3a644b2d22b0
Aditya Choudhary <choudharyaditya288@gmail.com>
2024-12-12 16:20:46 +0000

    Comparision discuss

---
+++ basics_01/04_comparision.js
@@ -0,0 +1,13 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 != 1);
+//console.log(2 == 1);
+
+//node basics_01/04_comparision.js
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
