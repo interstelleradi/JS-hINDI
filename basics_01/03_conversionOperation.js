d1af91bd050cf510d3c44ad8e550544e8dbbc86d
Aditya Choudhary <choudharyaditya288@gmail.com>
2024-12-12 08:50:34 +0000

    ConversionOperation

---
+++ basics_01/03_conversionOperation.js
@@ -0,0 +1,19 @@
+//let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+
+// "33"  => 33
+// "33abc"  => Nan
+//  true  => 1, False  => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
