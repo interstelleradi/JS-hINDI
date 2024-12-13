91517839e34eec81e457e30958dc9734f976a186
Aditya Choudhary <choudharyaditya288@gmail.com>
2024-12-13 13:00:18 +0000

    stack & Heap

--- basics_01/datatypes-summary.js
+++ basics_01/datatypes-summary.js
@@ -1,8 +1,8 @@
-// Primitive
+// 1Primitive
 
 // 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt.
 
-// for investigation ==  node datatypes-summary.js
+// for investigation ==  node basics_01/datatypes-summary.js
 
 const score = 100
 const scoreValue = 100.3
@@ -20,7 +20,7 @@ console.log(id === anotherId);
 
 
 
-// Reference ( Non Primitive)
+// 2Reference ( Non Primitive)
 
 // Array, Objects, Functions.
 
@@ -36,3 +36,27 @@ console.log("Hello world!");
 }
 
 console.log(typeof anotherId);
+
+
+
+
+// Stack (Primitive), Heap (Non- Primitive)
+
+let myYoutubename = "adityachoudharydotcom"
+
+let anothername = "trickyaditya"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "aditya@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
